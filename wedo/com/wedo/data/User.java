f47e658709e35a666aaf31705b4468737ae022bf
张永强 <824827715@qq.com>
2017-12-06 10:26:25 +0800

    基本环境搭建以及登录注册页面

---
+++ wedo/com/wedo/data/User.java
@@ -0,0 +1,44 @@
+package com.wedo.data;
+/* 
+ * source  data
+ * author  yq.zhang
+ * date    2017-12-1
+ * */
+public class User {
+	/*
+	 *主键 
+	 * */
+	private long id;
+	/*
+	 *账户名称 
+	 * */
+	private String username;
+	/*
+	 *密码 
+	 * */
+	private String miMa;
+	/*
+	 *手机号码 
+	 * */
+	private String phone;
+	/*
+	 *邮箱
+	 * */
+	private String email;
+	/*
+	 *性别
+	 * */
+	private int sex;
+	/*
+	 *状态
+	 * */
+	private int status;
+	/*
+	 *年龄 
+	 * */
+	private int age;
+	
+	
+	
+	
+}
